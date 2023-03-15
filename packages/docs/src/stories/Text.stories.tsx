e2723fa902ab72723de9b9907f561a70bec6cff5
Guilherme Ponte <guilhermescponte@hotmail.com>
2023-03-15 14:56:57 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cum voluptatibus perspiciatis explicabo, earum culpa consequuntur totam beatae, dolorem laboriosam assumenda odit in officiis cupiditate corporis sed cumque molestiae minima.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Heading',
+    as: 'strong',
+  },
+}
